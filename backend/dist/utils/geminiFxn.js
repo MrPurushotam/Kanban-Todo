2a27327362cc4186743ff383a8af73492b306dbc
purushotam <purupurushotamjeswani2004@gmail.com>
2025-05-24 10:36:33 +0530

    issue with deployment

--- backend/dist/utils/geminiFxn.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ai = void 0;
-const genai_1 = require("@google/genai");
-const apiKey = process.env.GEMINI_API_KEY;
-exports.ai = new genai_1.GoogleGenAI({ apiKey });
